7e5b9e2afdb2b4cb6824cc037c8a3db8f2e933bc
aameensiddiqui <aameensddq@gmail.com>
2024-12-11 18:38:57 +0530

    commit 5

--- day_5/Task 2/src/Components/Navbar.jsx
+++ day_5/Task 2/src/Components/Navbar.jsx
@@ -1,9 +1,11 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Link } from "react-router-dom"
 function Navbar() {
     return (
         <div>
             <nav className="navbar bg-success navbar-expand-lg" data-bs-theme="dark">
                 <div className="container-fluid">
+                    <i className="bi bi-code-slash fs-3 me-2 text-white"></i>
                     <Link className="navbar-brand" to="#">Day 5#</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
